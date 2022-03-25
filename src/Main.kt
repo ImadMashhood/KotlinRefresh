387e9716302505bb214228130bc51361ac23cbd8
Imad Mashhood <mashhoodimad@gmail.com>
2022-03-25 18:00:15 -0500

    Lambda Syntax Example Mutable

--- src/Main.kt
+++ src/Main.kt
@@ -15,9 +15,12 @@ fun main() {
 
     val things = mutableListOf("Birds", "Keyboards", "Food")
     things.add("Phone")
+    things.forEach { thing->
+        println(thing)
+    }
     val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
-    map.forEach { key, value ->  println("$key -> $value")}
     map.put(4, "d")
+    map.forEach { key, value ->  println("$key -> $value")}
 
 
 }
